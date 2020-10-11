c3e6d84428609b253595374711f61514d79e9c28
JC_onLine <jc.gillot@gmail.com>
2020-10-11 17:45:51 +0200

    feat(form);Add 'Equipage' form and test=ok with 3 hards code names;0.75

--- argonautes/views.py
+++ argonautes/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
+from .form import EquipageForm
+from .models import Equipage
 
 def main_page(request):
-    context = {}
+    form = EquipageForm(request.POST)
+    equipage = Equipage.objects.all()
+    # catch POST form
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            context = {
+                'equipage': equipage,
+                'form': form
+            }
+            return render(request, 'argonautes/index.html', context)
+    # display form
+    context = {
+        'equipage': equipage,
+        'form': form
+    }
     return render(request, 'argonautes/index.html', context)
